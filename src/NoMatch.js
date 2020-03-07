e60b28a6d6a05ac29149932a4198f7001eba1061
Robert Acquah <atominize@Roberts-MacBook.local>
2020-03-07 21:50:17 +0000

    Setup home, about and contact pages

---
+++ src/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function NoMatch() {
+    return (
+        <div>
+           < h1>Oops No Match</h1>
+            <p>
+                No Match
+            </p>
+        </div>
+    )
+}
